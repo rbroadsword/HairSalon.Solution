0f23e5971404243fe1ef3709f83231d66f94f933
Ryan Broadsword <rbroadsword@gmail.com>
2022-07-30 12:09:17 -0700

    add models for stylist, client, and hairsaloncontext

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+      public Stylist()
+        {
+            this.Clientss = new HashSet<Client>();
+        }
+
+        public int StylistId { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public virtual ICollection<Client> Clients { get; set; } 
+    }
+}
