047054e8c8bb016e22372d75ded5f9f873ab9ac9
Ryan Broadsword <rbroadsword@gmail.com>
2022-07-30 16:21:20 -0700

    fix errors in Controllers

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -45,7 +45,7 @@ namespace HairSalon.Controllers
       public ActionResult Edit(int id)
       {
         var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
-        ViewBag.StylistId = new SelectList(_db.Sttylists, "StylistId", "Name");
+        ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
         return View(thisClient);
       }
 
